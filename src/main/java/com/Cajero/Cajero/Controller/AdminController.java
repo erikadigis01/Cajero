9cc48eba45c3d29b3eaa3e7f0e86239e59bd4934
Sheylarp <rivera.sheyla.p@gmail.com>
2026-01-22 18:20:40 -0600

    rellenar cajero admin

---
+++ src/main/java/com/Cajero/Cajero/Controller/AdminController.java
@@ -0,0 +1,94 @@
+package com.Cajero.Cajero.Controller;
+
+import com.Cajero.Cajero.JPA.Cajero;
+import com.Cajero.Cajero.JPA.Result;
+import com.Cajero.Cajero.JPA.Usuario;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    public String url = "http://localhost:8080/";
+    
+    @GetMapping("/{idUsuario}")
+    public String perfilAdmin(@PathVariable("idUsuario") int idUsuario,
+                                Model model){
+        
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+        RestTemplate restTemplate = new RestTemplate();
+        
+        ResponseEntity<Result<Usuario>> response =
+        restTemplate.exchange(url + "/usuario/detail/" + idUsuario,
+                              HttpMethod.GET,
+                              entity,
+                              new ParameterizedTypeReference<Result<Usuario>>() {});
+        
+        ResponseEntity<Result<Cajero>> responseCajeros =
+        restTemplate.exchange(url + "/cajeros",
+                              HttpMethod.GET,
+                              entity,
+                              new ParameterizedTypeReference<Result<Cajero>>() {});
+        
+        
+        if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null
+                && responseCajeros.getStatusCode().is2xxSuccessful() && responseCajeros.getBody() != null) {
+            Usuario usuario = new Usuario();
+            usuario = (Usuario) response.getBody().object;
+            model.addAttribute("usuario", usuario);
+            
+            List<Cajero> cajeros = new ArrayList<>();
+            cajeros = (List<Cajero>) responseCajeros.getBody().objects;
+            model.addAttribute("cajeros", cajeros);
+        } else {
+        
+            model.addAttribute("error", "no se encontro este usuario");
+        }
+        return "Admin";
+    }
+    
+    @PostMapping("/rellenarCajero")
+    public String rellenarCajero(@RequestParam("inputIdCajero") int IdCajero,
+                                 @RequestParam("inputIdAdmin") int IdUser,
+                                 Model model, RedirectAttributes redirect){
+        
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+        RestTemplate restTemplate = new RestTemplate();
+        
+        ResponseEntity<Result> response =
+            restTemplate.exchange(
+                url + "cajeros/" + IdCajero,
+                HttpMethod.POST,
+                 entity,
+                new ParameterizedTypeReference<Result>() {}
+            );
+        Result result = response.getBody();
+        
+        if(response.getStatusCode().value() == 201) {
+            
+            redirect.addFlashAttribute("success", "Se relleno correctamente el cajero");
+        
+        } else {
+             redirect.addFlashAttribute("error", "No se relleno el cajero");
+        }
+        return "redirect:/admin/" + IdUser;
+    }
+
+}
